0d20777a6556bc89d8552d2667939d40b3bf7652
Multirious <multirious@outlook.com>
2023-04-05 23:39:42 +0700

    improve backend api

--- src/backend/mod.rs
+++ src/backend/mod.rs
@@ -1,9 +1,8 @@
 use once_cell::sync::Lazy;
-use std::sync::{RwLock, RwLockReadGuard};
 
 use crate::simulate::{KeyCommon, KeyLayout, MouseButton};
 
-use self::default::DefaultBackend;
+use parking_lot::{MappedRwLockReadGuard, RwLock, RwLockReadGuard};
 
 pub mod default;
 #[cfg(feature = "enigo")]
@@ -24,27 +23,34 @@ pub trait KeyboardBackend<K> {
     fn key_up(&self, key: K);
 }
 
-type StaticBackend<T> = Lazy<RwLock<Box<T>>>;
-type BackendRead<T> = RwLockReadGuard<'static, Box<T>>;
+type StaticBackend<T> = Lazy<RwLock<Option<Box<T>>>>;
+type BackendRead<T> = MappedRwLockReadGuard<'static, Box<T>>;
 
 macro_rules! define_backend_static {
     (
         $(
         static $static_ident:ident: $trait:ty;
-        pub fn $fn_get:ident();
+        pub fn $fn_get:ident(), expect_message = $expect_message:expr;
         pub fn $fn_set:ident();
+        pub fn $fn_take:ident();
         )*
     ) => {
         $(
             static $static_ident: StaticBackend<$trait> =
-                Lazy::new(|| RwLock::new(Box::new(DefaultBackend)));
+                Lazy::new(|| RwLock::new(None));
 
             pub fn $fn_get() -> BackendRead<$trait> {
-                $static_ident.read().unwrap()
+                RwLockReadGuard::map($static_ident.read(), |backend| {
+                    backend.as_ref().expect($expect_message)
+                })
             }
 
             pub fn $fn_set(backend: Box<$trait>) {
-                *$static_ident.write().unwrap() = backend;
+                let _ = $static_ident.write().insert(backend);
+            }
+
+            pub fn $fn_take() -> Option<Box<$trait>> {
+                $static_ident.write().take()
             }
         )*
     };
@@ -52,14 +58,17 @@ macro_rules! define_backend_static {
 
 define_backend_static! {
     static MOUSE_BACKEND: dyn MouseBackend + Send + Sync;
-    pub fn get_mouse_backend();
+    pub fn get_mouse_backend(), expect_message = "No mouse backend";
     pub fn set_mouse_backend();
+    pub fn take_mouse_backend();
 
     static KEYBOARD_COMMON_BACKEND: dyn KeyboardBackend<KeyCommon> + Send + Sync;
-    pub fn get_keyboard_common_backend();
+    pub fn get_keyboard_common_backend(), expect_message = "No keyboard common backend";
     pub fn set_keyboard_common_backend();
+    pub fn take_keyboard_common_backend();
 
     static KEYBOARD_LAYOUT_BACKEND: dyn KeyboardBackend<KeyLayout> + Send + Sync;
-    pub fn get_keyboard_layout_backend();
+    pub fn get_keyboard_layout_backend(), expect_message = "No keyboard layout backend";
     pub fn set_keyboard_layout_backend();
+    pub fn take_keyboard_layout_backend();
 }
